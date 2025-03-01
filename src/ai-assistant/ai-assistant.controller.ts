7906260bdfd6f11a0d6ae145e049d9d2d3bb1c9e
codewithalihamza <512syedalihamza@gmail.com>
2025-03-01 20:23:42 +0500

    Create AI Assistant Module and Integrate Gemini for generate ai text

---
+++ src/ai-assistant/ai-assistant.controller.ts
@@ -0,0 +1,17 @@
+import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { AiAssistantService } from './ai-assistant.service';
+import { CreateAIMessageDto } from './dto/create-ai-message.dto';
+
+@Controller('ai_assistant')
+export class AiAssistantController {
+  constructor(private readonly aiAssistantService: AiAssistantService) { }
+
+  @Post("chat")
+  @UsePipes(new ValidationPipe({ transform: true }))
+  createAIMessage(
+    @Body() createAIMessageDto: CreateAIMessageDto
+  ) {
+    return this.aiAssistantService.generateText(createAIMessageDto)
+  }
+
+}
