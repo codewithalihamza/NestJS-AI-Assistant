cd59ac52fd75615f7f367a3a3d0cc3be83589799
codewithalihamza <512syedalihamza@gmail.com>
2025-03-01 12:50:00 +0500

    enable cors and update the configuration in main.ts file

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const port = process.env.PORT
+  const port: number = +process.env.PORT;
   const app = await NestFactory.create(AppModule);
-  await app.listen(port);
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3001',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  await app.listen(port, () => console.log(`Nest server ready at ${port}`));
 }
 bootstrap();
